4e93f86af54ed3b180604b5ddbfb2f86f68db292
unknown <diogo322@hotmail.com>
2024-01-05 23:17:28 -0300

    First Commit

---
+++ Controle_Grupo/Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{ 
+    public static void Main(string[] args)
+    {
+        string[] grupo = { "James", "Nathan", "Salomon", "Mayer", "Napoleão", "Wellington" };
+        
+        int soma = 0; 
+
+        for (int i = 0; i < grupo.Length; i++)
+        {
+            if (grupo.Length > 9)
+            {
+                Console.WriteLine($"O grupo atingiu o número máximo de pessoas, por favor dirija-se ao próximo avião");
+                break;
+            }
+            soma ++;
+           Console.WriteLine($"Número de passageiros: {soma}");
+           Console.WriteLine($"O grupo pode embarcar no avião");
+        }
+        
+    }
+}
